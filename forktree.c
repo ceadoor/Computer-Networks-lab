91c6f64bdafcbc73e6310f225a8d016a1e34ee2e
NajafMohammed <45817643+najafmohammed@users.noreply.github.com>
2020-02-17 14:24:36 +0530

    Update forktree.c

--- forktree.c
+++ forktree.c
@@ -3,28 +3,9 @@
 
 void main()
 {
-    int pId1,pId2;
+    int pId1;
     pId1 = fork();
-    pId2 = fork();
     if (pId1 < 0)
-    {
-        printf("Process creation is Unsuccessful!");
-    }
-    // The return value is 0 for a child process
-    else if (pId2 == 0)
-    {
-        printf("Child process:");
-        printf("\nChild : Child’s PID: %d", getpid());
-        printf("\nChild : Parent’s PID: %d\n", getppid());
-    }
-    // The return value is positive for a parent process
-    else if (pId2 > 0)
-    {
-        printf("Parent process:");
-        printf("\nParent : Parent’s PID: %d", getpid());
-        printf("\nParent : Child’s PID: %d\n", pId2);
-    }
-    if (pId2 < 0)
     {
         printf("Process creation is Unsuccessful!");
     }
@@ -34,6 +15,26 @@ void main()
         printf("Child process:");
         printf("\nChild : Child’s PID: %d", getpid());
         printf("\nChild : Parent’s PID: %d\n", getppid());
+        int pId2;
+        pId2 = fork();
+        if (pId2 < 0)
+   	 {
+       	 	printf("Process creation is Unsuccessful!");
+   	 }
+   	 // The return value is 0 for a child process
+   	 else if (pId2 == 0)
+    	{
+    	    printf("Child process:");
+    	    printf("\nChild : Child’s PID: %d", getpid());
+   	     printf("\nChild : Parent’s PID: %d\n", getppid());
+   	 }
+   	 // The return value is positive for a parent process
+   	 else if (pId2 > 0)
+   	 {
+     	   printf("Parent process:");
+     	   printf("\nParent : Parent’s PID: %d", getpid());
+     	   printf("\nParent : Child’s PID: %d\n", pId2);
+    	}
     }
     // The return value is positive for a parent process
     else if (pId1 > 0)
